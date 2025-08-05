d9abee4ac2f62f0496c106e3e8f080e3ed602418
irinabaker <paxpistor@gmail.com>
2025-08-05 12:57:08 +0200

    Updated

--- src/test/java/com/ait/qa63/FirstSeleniumTest.java
+++ src/test/java/com/ait/qa63/FirstSeleniumTest.java
@@ -2,17 +2,46 @@ package com.ait.qa63;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class FirstSeleniumTest {
 
     WebDriver driver;
 
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("BeforeSuite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("AfterSuite");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("*****BeforeTest");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("*****AfterTest");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("**********BeforeClass");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("**********AfterClass");
+    }
+
     //before - setUp
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
+        System.out.println("***************BeforeMethod");
         driver = new ChromeDriver();
       //  driver.get("https://www.google.com");//without history
         driver.navigate().to("https://www.google.com");//with history
@@ -21,13 +50,14 @@ public class FirstSeleniumTest {
         driver.navigate().refresh();
     }
     //test
-    @Test
+    @Test()
     public void firstTest() {
-        System.out.println("Google is opened!");
+        System.out.println("*******************Test");System.out.println("Google is opened!");
     }
     //after - tearDown
-    @AfterMethod(enabled = false)
+    @AfterMethod(enabled = true)
     public void tearDown() {
+        System.out.println("***************AfterMethod");
       //  driver.quit();// all tabs & close browser
         driver.close();// only one tab (if tab only one -> close browser)
     }
